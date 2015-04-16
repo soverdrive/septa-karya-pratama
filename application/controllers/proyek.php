05426b8faee36bef437e14429198ae1ccc8afbfd
Muhammad Agil Mahbuby <wesker9110@gmail.com>
2015-04-16 22:16:30 +0700

    benerin galeri

--- application/controllers/proyek.php
+++ application/controllers/proyek.php
@@ -14,13 +14,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class proyek extends CI_Controller {
 
-    public function index() {
+    public function index() 
+	{
         $head['title'] = "Proyek";
         $this->load->view('v_head', $head);
+		$data['keterangan'] = array();
+		// nambah keterangan nya di sini, untuk akses di view nya tinggal panggil variable 
+		// $keterangan dan disertai index nya misal $keterangan[0]
+		// NB : keterangan dalam bentuk HTML, nanti ditampilkan di bawah gambar saat gambar di klik
+		#region tambah keterangan dalam bentuk array
+		
+		// data ke-1
+		$data['keterangan'] [] = "
+		<u>Lorem Ipsum Dolor Sit Amet</u>
+		<br>
+		Lorem ipsum dolor sit amet
+		";
+		// data ke-2
+		$data['keterangan'] [] = "
+		<u>Sample Text</u>
+		<br>
+		Sample text here for you
+		";
+		#endregion
         $array_navbar['active'] = 'proyek';
         $this->load->view('v_navbar', $array_navbar);
         $this->load->view('v_header');
-        $this->load->view('proyek/v_portfolio');
+        $this->load->view('proyek/v_portfolio', $data);
         $this->load->view('v_footer');
     }
 }
